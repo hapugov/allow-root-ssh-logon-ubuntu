be381ed87ec86f145b31d5796fb9667c22e2454a
hapugov <107251908+hapugov@users.noreply.github.com>
2024-05-03 14:06:56 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config && sudo systemctl restart ssh &&sudo passwd root
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config && sudo systemctl restart ssh && sudo passwd root
